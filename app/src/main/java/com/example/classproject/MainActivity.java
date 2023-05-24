18148e505502657bf499040b686dc4e674275b17
Keddmon <dh03219@naver.com>
2023-05-24 22:17:54 +0900

    third

--- app/src/main/java/com/example/classproject/MainActivity.java
+++ app/src/main/java/com/example/classproject/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
@@ -12,35 +15,62 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView mRv_inventory; // m = member
-    private FloatingActionButton mBtn_write;
-    private ArrayList<InventoryItem> mInventoryItems;
+    private TableLayout tableLayout;
     private DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        하정훈 병신
 
+        tableLayout = findViewById(R.id.tableLayout);
         dbHelper = new DBHelper(this);
+
+        // 데이터베이스에 새로운 데이터 삽입 예시
+        dbHelper.InsertInventory(1, "음료수", 10);
+        dbHelper.InsertInventory(2, "과자", 15);
+
+        dbHelper.UpdateInventory(1, "환타", 15);
+        dbHelper.UpdateInventory(2, "맛동산", 20);
+
+        dbHelper.DeleteInventory(1);
+
+        updateTable();
+    }
+    private void updateTable() {
+
+        // DB에서 모든 상품 정보 가져오기
         ArrayList<InventoryItem> inventoryItems = dbHelper.getInventoryItems();
 
-
-        dbHelper.InsertInventory(1, "상품1", 10);
-        dbHelper.InsertInventory(2, "상품2", 20);
-
-        dbHelper.UpdateInventory(1, "상품1", 15);
-        dbHelper.UpdateInventory(2, "상품2", 30);
-
-        // 로그로 상품 정보 출력
+        // 각각의 상품 정보에 대해 TableRow을 생성하여 표에 추가
         for (InventoryItem item : inventoryItems) {
-            Log.d("Inventory", "ID: " + item.getId());
-            Log.d("Inventory", "Name: " + item.getName());
-            Log.d("Inventory", "Quantity: " + item.getQuantity());
-            Log.d("Inventory", "Sales: " + item.getSales());
-            Log.d("Inventory", "Stock: " + item.getStock());
-            Log.d("Inventory", "Date: " + item.getDate());
+            TableRow tableRow = new TableRow(this);
+
+            TextView textViewId = new TextView(this);
+            textViewId.setText(String.valueOf(item.getId()));
+            tableRow.addView(textViewId);
+
+            TextView textViewName = new TextView(this);
+            textViewName.setText(item.getName());
+            tableRow.addView(textViewName);
+
+            TextView textViewQuantity = new TextView(this);
+            textViewQuantity.setText(String.valueOf(item.getQuantity()));
+            tableRow.addView(textViewQuantity);
+
+            TextView textViewSales = new TextView(this);
+            textViewSales.setText(String.valueOf(item.getSales()));
+            tableRow.addView(textViewSales);
+
+            TextView textViewStock = new TextView(this);
+            textViewStock.setText(String.valueOf(item.getStock()));
+            tableRow.addView(textViewStock);
+
+            TextView textViewDate = new TextView(this);
+            textViewDate.setText(item.getDate());
+            tableRow.addView(textViewDate);
+
+            tableLayout.addView(tableRow);
         }
     }
 }
